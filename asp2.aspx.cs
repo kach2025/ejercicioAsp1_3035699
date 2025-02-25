e870b864990b8e00170a52210adee721d5cb779a
kach2025 <acampos1905@gmail.com>
2025-02-25 15:29:28 -0600

    segundo commit

---
+++ asp2.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ejercicioAsp1_3035699
+{
+    public partial class asp2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddlCategory.SelectedValue = Request.Cookies["ddlCategory"].Value;
+            ddlSupplier.SelectedValue = Request.Cookies["ddlSupplier"].Value;
+            strProduct.Text = Request.Cookies["strProduct"].Value;
+            strDescription.Text = Request.Cookies["strDescription"].Value;
+            strImage.Text = Request.Cookies["strImage"].Value;
+            decimal DecPrice = Convert.ToDecimal(Request.Cookies["decPrice"].Value);
+            decPrice.Text = DecPrice.ToString("c");
+            lblNumberInStrock.Text = Request.Cookies["bytNumberInStock"].Value;
+            lblNumberOnOrder.Text = Request.Cookies["bytNumberOnOrder"].Value;
+            bytReorderLevel.Text = Request.Cookies["bytReorderLevel"].Value;
+
+            Byte byNumberInStock = Convert.ToByte(Request.Cookies["bytNumberInStock"].Value);
+            Byte byNumberOnOrder = Convert.ToByte(Request.Cookies["bytNumberOnOrder"].Value);
+            decimal decValueInStock = DecPrice * byNumberInStock;
+            decimal decValueOnOrder = DecPrice * byNumberOnOrder;
+            bytNumberInStock.Text = " (Value: " + decValueInStock.ToString("c") + ")";
+            bytNumberOnOrder.Text = " (Value: " + decValueOnOrder.ToString("c") + ")";
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Asp1.aspx");
+        }
+    }
+}
---
+++ asp2.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ejercicioAsp1_3035699
+{
+
+
+    public partial class asp2
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control ddlCategory.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategory;
+
+        /// <summary>
+        /// Control ddlSupplier.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSupplier;
+
+        /// <summary>
+        /// Control strProduct.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox strProduct;
+
+        /// <summary>
+        /// Control strDescription.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox strDescription;
+
+        /// <summary>
+        /// Control strImage.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox strImage;
+
+        /// <summary>
+        /// Control decPrice.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox decPrice;
+
+        /// <summary>
+        /// Control lblNumberInStrock.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNumberInStrock;
+
+        /// <summary>
+        /// Control bytNumberInStock.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox bytNumberInStock;
+
+        /// <summary>
+        /// Control lblNumberOnOrder.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNumberOnOrder;
+
+        /// <summary>
+        /// Control bytNumberOnOrder.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox bytNumberOnOrder;
+
+        /// <summary>
+        /// Control bytReorderLevel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox bytReorderLevel;
+
+        /// <summary>
+        /// Control btnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSave;
+
+        /// <summary>
+        /// Control btnBack.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBack;
+    }
+}
